18c8a59a1ee4bc8f48102a310fe664c086f90459
rumman <139452817+Rummantouqeer@users.noreply.github.com>
2023-10-10 09:54:46 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter your sentence to count the word",
+    }
+]);
+const wordcount = answers.sentence.split(" ");
+console.log(wordcount);
+console.log(`Your sentence word count is ${wordcount.length}`);
